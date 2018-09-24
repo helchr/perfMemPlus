ac6de4b0a2f130f6c8424cdca7a7e644a3426f11
helchr <36977302+helchr@users.noreply.github.com>
2018-09-24 23:19:35 +0900

    Add all dependencies to buildAll

--- buildAll.sh
+++ buildAll.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+#install dependencies for qt based applications
+apt-get install g++ make qtbase5-dev qt5-default libqt5charts5-dev
+
+#build perfMemPlus components
 cd allocationTracker
 make
 cd ..
 cd prepareDatabase
-qmake prepareDatabase
-make qmake_all
+qmake prepareDatabase.pro
 make
 cd ..
 cd viewer
 qmake viewer.pro
-make qmake_all
 make
 cd ..
-sudo apt-get install linux-source-4.8.0
-tar jxf /usr/src/linux-source-.tar.bz2
-cd linux-source-/tools/perf/
+
+#install dependencies for building perf with required modules
+apt-get install flex bison libelf-dev libnuma-dev libunwind-dev elfutils libdw-dev python-dev binutils-dev libbfd-dev linux-tools-$(uname -r)
+
+#build perf
+apt-get install linux-source
+tar jxf /usr/src/linux-source*.tar.bz2
+cd linux-source-*/tools/perf
 make
 make install
+cd ..
+rm -r -f linux-source-*/
+
 
