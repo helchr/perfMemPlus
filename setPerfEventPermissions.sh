af81c580e1e52457722fa06d1590dee504cd69c7
helchr <36977302+helchr@users.noreply.github.com>
2020-08-20 18:28:50 +0900

    Update setPerfEventPermissions.sh

--- setPerfEventPermissions.sh
+++ setPerfEventPermissions.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-chmod -f +x perfMemPlus
-chmod -f +x perf
-chmod -f +x viewer/viewer
-chmod -f +x prepareDatabase/prepareDatabase
 sudo sh -c "echo -1 > /proc/sys/kernel/perf_event_paranoid"
